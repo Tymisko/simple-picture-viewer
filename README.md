5e124683964950682397a1a5481f840530234a7b
Jan Urbaś <janurbas39@gmail.com>
2022-03-24 07:33:49 +0100

    Initial commit with README.md and .gitignore

---
+++ README.md
@@ -0,0 +1,3 @@
+# Simple Picture Viewer
+
+Windows Forms application that allow you to select your picture and display it in the window.
---
+++ SimplePictureViewer/Form1.cs
@@ -0,0 +1,10 @@
+namespace SimplePictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
